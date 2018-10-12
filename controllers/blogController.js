47f639d9d8817bc4839675f7640715b278dda856
nitin095 <nitinshokeen9@gmail.com>
2018-10-13 01:45:25 +0530

    final

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,10 +1,15 @@
-const express = require('express')
 const mongoose = require('mongoose');
 const shortid = require('shortid');
-
-//Importing the model here 
+const time = require('./../libs/timeLib');
+const response = require('./../libs/responseLib')
+const logger = require('./../libs/loggerLib');
+const check = require('./../libs/checkLib')
+/* Models */
 const BlogModel = mongoose.model('Blog')
 
+/**
+ * function to read all blogs.
+ */
 let getAllBlog = (req, res) => {
     BlogModel.find()
         .select('-__v -_id')
@@ -12,12 +17,16 @@ let getAllBlog = (req, res) => {
         .exec((err, result) => {
             if (err) {
                 console.log(err)
-                res.send(err)
-            } else if (result == undefined || result == null || result == '') {
-                console.log('No Blog Found')
-                res.send("No Blog Found")
+                logger.error(err.message, 'Blog Controller: getAllBlog', 10)
+                let apiResponse = response.generate(true, 'Failed To Find Blog Details', 500, null)
+                res.send(apiResponse)
+            } else if (check.isEmpty(result)) {
+                logger.info('No Blog Found', 'Blog Controller: getAllBlog')
+                let apiResponse = response.generate(true, 'No Blog Found', 404, null)
+                res.send(apiResponse)
             } else {
-                res.send(result)
+                let apiResponse = response.generate(false, 'All Blog Details Found', 200, result)
+                res.send(apiResponse)
             }
         })
 }// end get all blogs
@@ -26,20 +35,34 @@ let getAllBlog = (req, res) => {
  * function to read single blog.
  */
 let viewByBlogId = (req, res) => {
-    
-    BlogModel.findOne({ 'blogId': req.params.blogId }, (err, result) => {
 
-        if (err) {
-            console.log(err)
-            res.send(err)
-        } else if (result == undefined || result == null || result == '') {
-            console.log('No Blog Found')
-            res.send("No Blog Found")
-        } else {
-            res.send(result)
+    if (check.isEmpty(req.params.blogId)) {
 
-        }
-    })
+        console.log('blogId should be passed')
+        let apiResponse = response.generate(true, 'blogId is missing', 403, null)
+        res.send(apiResponse)
+    } else {
+
+        BlogModel.findOne({ 'blogId': req.params.blogId }, (err, result) => {
+
+            if (err) {
+
+                console.log('Error Occured.')
+                logger.error(`Error Occured : ${err}`, 'Database', 10)
+                let apiResponse = response.generate(true, 'Error Occured.', 500, null)
+                res.send(apiResponse)
+            } else if (check.isEmpty(result)) {
+
+                console.log('Blog Not Found.')
+                let apiResponse = response.generate(true, 'Blog Not Found', 404, null)
+                res.send(apiResponse)
+            } else {
+                logger.info("Blog found successfully","BlogController:ViewBlogById",5)
+                let apiResponse = response.generate(false, 'Blog Found Successfully.', 200, result)
+                res.send(apiResponse)
+            }
+        })
+    }
 }
 
 /**
@@ -47,19 +70,33 @@ let viewByBlogId = (req, res) => {
  */
 let viewByCategory = (req, res) => {
 
-    BlogModel.find({ 'category': req.params.category }, (err, result) => {
+    if (check.isEmpty(req.params.categoryId)) {
 
-        if (err) {
-            console.log(err)
-            res.send(err)
-        } else if (result == undefined || result == null || result == '') {
-            console.log('No Blog Found')
-            res.send("No Blog Found")
-        } else {
-            res.send(result)
+        console.log('categoryId should be passed')
+        let apiResponse = response.generate(true, 'CategoryId is missing', 403, null)
+        res.send(apiResponse)
+    } else {
 
-        }
-    })
+        BlogModel.find({ 'category': req.params.category }, (err, result) => {
+
+            if (err) {
+
+                console.log('Error Occured.')
+                logger.error(`Error Occured : ${err}`, 'Database', 10)
+                let apiResponse = response.generate(true, 'Error Occured.', 500, null)
+                res.send(apiResponse)
+            } else if (check.isEmpty(result)) {
+
+                console.log('Blogs Not Found.')
+                let apiResponse = response.generate(true, 'Blogs Not Found', 404, null)
+                res.send(apiResponse)
+            } else {
+                console.log('Blogs Found Successfully')
+                let apiResponse = response.generate(false, 'Blogs Found Successfully.', 200, result)
+                res.send(apiResponse)
+            }
+        })
+    }
 }
 
 /**
@@ -67,19 +104,33 @@ let viewByCategory = (req, res) => {
  */
 let viewByAuthor = (req, res) => {
 
-    BlogModel.find({ 'author': req.params.author }, (err, result) => {
+    if (check.isEmpty(req.params.author)) {
 
-        if (err) {
-            console.log(err)
-            res.send(err)
-        } else if (result == undefined || result == null || result == '') {
-            console.log('No Blog Found')
-            res.send("No Blog Found")
-        } else {
-            res.send(result)
+        console.log('author should be passed')
+        let apiResponse = response.generate(true, 'author is missing', 403, null)
+        res.send(apiResponse)
+    } else {
 
-        }
-    })
+        BlogModel.find({ 'author': req.params.author }, (err, result) => {
+
+            if (err) {
+
+                console.log('Error Occured.')
+                logger.error(`Error Occured : ${err}`, 'Database', 10)
+                let apiResponse = response.generate(true, 'Error Occured.', 500, null)
+                res.send(apiResponse)
+            } else if (check.isEmpty(result)) {
+
+                console.log('Blogs Not Found.')
+                let apiResponse = response.generate(true, 'Blogs Not Found', 404, null)
+                res.send(apiResponse)
+            } else {
+                console.log('Blogs Found Successfully')
+                let apiResponse = response.generate(false, 'Blogs Found Successfully.', 200, result)
+                res.send(apiResponse)
+            }
+        })
+    }
 }
 
 /**
@@ -87,113 +138,212 @@ let viewByAuthor = (req, res) => {
  */
 let editBlog = (req, res) => {
 
-    let options = req.body;
-    console.log(options);
-    BlogModel.update({ 'blogId': req.params.blogId }, options, { multi: true }).exec((err, result) => {
+    if (check.isEmpty(req.params.blogId)) {
 
-        if (err) {
-            console.log(err)
-            res.send(err)
-        } else if (result == undefined || result == null || result == '') {
-            console.log('No Blog Found')
-            res.send("No Blog Found")
-        } else {
-            res.send(result)
+        console.log('blogId should be passed')
+        let apiResponse = response.generate(true, 'blogId is missing', 403, null)
+        res.send(apiResponse)
+    } else {
 
-        }
-    })
+        let options = req.body;
+        console.log(options);
+        BlogModel.update({ 'blogId': req.params.blogId }, options, { multi: true }).exec((err, result) => {
+
+            if (err) {
+
+                console.log('Error Occured.')
+                logger.error(`Error Occured : ${err}`, 'Database', 10)
+                let apiResponse = response.generate(true, 'Error Occured.', 500, null)
+                res.send(apiResponse)
+            } else if (check.isEmpty(result)) {
+
+                console.log('Blog Not Found.')
+                let apiResponse = response.generate(true, 'Blog Not Found', 404, null)
+                res.send(apiResponse)
+            } else {
+                console.log('Blog Edited Successfully')
+                let apiResponse = response.generate(false, 'Blog Edited Successfully.', 200, result)
+                res.send(apiResponse)
+            }
+        })
+    }
 }
 
+/**
+ * function to find the assignment.
+ */
+let findBlogToEdit = (blogId) => {
 
+    if (check.isEmpty(req.params.blogId)) {
+
+        console.log('blogId should be passed')
+        let apiResponse = response.generate(true, 'blogId is missing', 403, null)
+        reject(apiResponse)
+    } else {
+        return new Promise((resolve, reject) => {
+            BlogModel.findOne({ 'blogId': req.params.blogId }, (err, blog) => {
+                if (err) {
+                    console.log('Error Occured.')
+                    logger.error(`Error Occured : ${err}`, 'Database', 10)
+                    let apiResponse = response.generate(true, 'Error Occured.', 500, null)
+                    reject(apiResponse)
+                } else if (check.isEmpty(blog)) {
+                    console.log('Blog Not Found.')
+                    let apiResponse = response.generate(true, 'Blog Not Found', 404, null)
+                    reject(apiResponse)
+                } else {
+                    console.log('Blog Found.')
+                    resolve(blog)
+                }
+            })
+        })
+    }
+}
 
 /**
  * function to delete the assignment collection.
  */
 let deleteBlog = (req, res) => {
-    BlogModel.remove({ 'blogId': req.params.blogId }, (err, result) => {
-        if (err) {
-            console.log(err)
-            res.send(err)
-        } else if (result == undefined || result == null || result == '') {
-            console.log('No Blog Found')
-            res.send("No Blog Found")
-        } else {
-            res.send(result)
 
-        }
-    })
+    if (check.isEmpty(req.params.blogId)) {
+
+        console.log('blogId should be passed')
+        let apiResponse = response.generate(true, 'blogId is missing', 403, null)
+        res.send(apiResponse)
+    } else {
+
+        BlogModel.remove({ 'blogId': req.params.blogId }, (err, result) => {
+            if (err) {
+                console.log('Error Occured.')
+                logger.error(`Error Occured : ${err}`, 'Database', 10)
+                let apiResponse = response.generate(true, 'Error Occured.', 500, null)
+                res.send(apiResponse)
+            } else if (check.isEmpty(result)) {
+                console.log('Blog Not Found.')
+                let apiResponse = response.generate(true, 'Blog Not Found.', 404, null)
+                res.send(apiResponse)
+            } else {
+                console.log('Blog Deletion Success')
+                let apiResponse = response.generate(false, 'Blog Deleted Successfully', 200, result)
+                res.send(apiResponse)
+            }
+        })
+    }
 }
 
 /**
  * function to create the blog.
  */
 let createBlog = (req, res) => {
-    var today = Date.now()
-    let blogId = shortid.generate()
+    let blogCreationFunction = () => {
+        return new Promise((resolve, reject) => {
+            console.log(req.body)
+            if (check.isEmpty(req.body.title) || check.isEmpty(req.body.description) || check.isEmpty(req.body.blogBody) || check.isEmpty(req.body.category)) {
 
-    let newBlog = new BlogModel({
+                console.log("403, forbidden request");
+                let apiResponse = response.generate(true, 'required parameters are missing', 403, null)
+                reject(apiResponse)
+            } else {
 
-        blogId: blogId,
-        title: req.body.title,
-        description: req.body.description,
-        bodyHtml: req.body.blogBody,
-        isPublished: true,
-        category: req.body.category,
-        author: req.body.fullName,
-        created: today,
-        lastModified: today
-    }) // end new blog model
+                var today = Date.now()
+                let blogId = shortid.generate()
 
-    let tags = (req.body.tags != undefined && req.body.tags != null && req.body.tags != '') ? req.body.tags.split(',') : []
-    newBlog.tags = tags
+                let newBlog = new BlogModel({
 
-    newBlog.save((err, result) => {
-        if (err) {
-            console.log(err)
-            res.send(err)
-        } else {
-            res.send(result)
+                    blogId: blogId,
+                    title: req.body.title,
+                    description: req.body.description,
+                    bodyHtml: req.body.blogBody,
+                    isPublished: true,
+                    category: req.body.category,
+                    author: req.user.fullName,
+                    created: today,
+                    lastModified: today
+                }) // end new blog model
 
-        }
-    }) // end new blog save
+                let tags = (req.body.tags != undefined && req.body.tags != null && req.body.tags != '') ? req.body.tags.split(',') : []
+                newBlog.tags = tags
+
+                newBlog.save((err, result) => {
+                    if (err) {
+                        console.log('Error Occured.')
+                        logger.error(`Error Occured : ${err}`, 'Database', 10)
+                        let apiResponse = response.generate(true, 'Error Occured.', 500, null)
+                        reject(apiResponse)
+                    } else {
+                        console.log('Success in blog creation')
+                        resolve(result)
+                    }
+                }) // end new blog save
+            }
+        }) // end new blog promise
+    } // end create blog function
+
+    // making promise call.
+    blogCreationFunction()
+        .then((result) => {
+            let apiResponse = response.generate(false, 'Blog Created successfully', 200, result)
+            res.send(apiResponse)
+        })
+        .catch((error) => {
+            console.log(error)
+            res.send(error)
+        })
 }
 
 /**
  * function to increase views of a blog.
  */
 let increaseBlogView = (req, res) => {
-
-    BlogModel.findOne({ 'blogId': req.params.blogId }, (err, result) => {
-
-        if (err) {
-            console.log(err)
-            res.send(err)
-        } else if (result == undefined || result == null || result == '') {
-            console.log('No Blog Found')
-            res.send("No Blog Found")
+    
+        if (check.isEmpty(req.params.blogId)) {
+    
+            console.log('blogId should be passed')
+            let apiResponse = response.generate(true, 'blogId is missing', 403, null)
+            res.send(apiResponse)
         } else {
-
-            result.views += 1;
-            result.save(function (err, result) {
+    
+            BlogModel.findOne({ 'blogId': req.params.blogId }, (err, result) => {
+    
                 if (err) {
-                    console.log(err)
-                    res.send(err)
+    
+                    console.log('Error Occured.')
+                    logger.error(`Error Occured : ${err}`, 'Database', 10)
+                    let apiResponse = response.generate(true, 'Error Occured.', 500, null)
+                    res.send(apiResponse)
+                } else if (check.isEmpty(result)) {
+    
+                    console.log('Blog Not Found.')
+                    let apiResponse = response.generate(true, 'Blog Not Found', 404, null)
+                    res.send(apiResponse)
+                } else {
+                    result.views += 1;
+                    result.save(function(err,result){
+                        if(err){
+                            console.log('Error Occured.')
+                            logger.error(`Error Occured : ${err}`, 'Database', 10)
+                            let apiResponse = response.generate(true, 'Error Occured While saving blog', 500, null)
+                            res.send(apiResponse)
+                        }
+                        else{
+                            console.log('Blog Updated Successfully')
+                            let apiResponse = response.generate(false, 'Blog Updated Successfully.', 200, result)
+                            res.send(apiResponse)
+                        }
+                    });// end result
+                    
                 }
-                else {
-                    console.log("Blog updated successfully")
-                    res.send(result)
-
-                }
-            });// end result
-
+            })
         }
-    })
-}
+    }
 
+ 
+    
 
 
 
 module.exports = {
+
     getAllBlog: getAllBlog,
     createBlog: createBlog,
     viewByBlogId: viewByBlogId,
@@ -201,5 +351,5 @@ module.exports = {
     viewByAuthor: viewByAuthor,
     editBlog: editBlog,
     deleteBlog: deleteBlog,
-    increaseBlogView: increaseBlogView
-}
+    increaseBlogView : increaseBlogView
+}// end exports
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // this is needed for importing expressjs into our application
 const express = require('express')
+const http = require('http')
 const appConfig = require('./config/appConfig')
 const fs = require('fs')
 const mongoose = require('mongoose')
@@ -7,6 +8,8 @@ const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const globalErrorMiddleware = require('./middlewares/appErrorHandler')
 const routeLoggerMiddleware = require('./middlewares/routeLogger')
+var helmet = require('helmet')
+const logger = require('./libs/loggerLib')
 
 
 //declaring an instance or creating an application instance
@@ -19,6 +22,7 @@ app.use(cookieParser())
 
 app.use(globalErrorMiddleware.globalErrorHandler)
 app.use(routeLoggerMiddleware.logIp)
+app.use(helmet())
 
 
 
@@ -29,8 +33,8 @@ fs.readdirSync(modelsPath).forEach(function (file) {
         console.log(file)
         require(modelsPath + '/' + file)
     }
-  })
-  // end Bootstrap models
+})
+// end Bootstrap models
 
 
 
@@ -47,19 +51,67 @@ fs.readdirSync(routesPath).forEach(function (file) {
 // end bootstrap route
 
 
-  // calling global 404 handler after route
+// calling global 404 handler after route
 
-  app.use(globalErrorMiddleware.globalNotFoundHandler)
+app.use(globalErrorMiddleware.globalNotFoundHandler)
 
-  // end global 404 handler
+// end global 404 handler
+/**
+ * Create HTTP server.
+ */
 
+const server = http.createServer(app)
+// start listening to http server
+console.log(appConfig)
+server.listen(appConfig.port)
+server.on('error', onError)
+server.on('listening', onListening)
 
-//listening the server - creating a local server
-app.listen(appConfig.port, () => {
-    console.log('Example app listening on port 3000!');
-    //creating the mongo db connection here
-    let db = mongoose.connect(appConfig.db.uri, { useMongoClient: true });
+// end server listening code
 
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        logger.error(error.code + ' not equal listen', 'serverOnErrorHandler', 10)
+        throw error
+    }
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            logger.error(error.code + ':elavated privileges required', 'serverOnErrorHandler', 10)
+            process.exit(1)
+            break
+        case 'EADDRINUSE':
+            logger.error(error.code + ':port is already in use.', 'serverOnErrorHandler', 10)
+            process.exit(1)
+            break
+        default:
+            logger.error(error.code + ':some unknown error occured', 'serverOnErrorHandler', 10)
+            throw error
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+    var addr = server.address()
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    ('Listening on ' + bind)
+    logger.info('server listening on port' + addr.port, 'serverOnListeningHandler', 10)
+    let db = mongoose.connect(appConfig.db.uri, { useMongoClient: true })
+}
+
+process.on('unhandledRejection', (reason, p) => {
+    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason)
+    // application specific logging, throwing an error, or other logic here
 })
 
 
